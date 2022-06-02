847d9dbe93f91ae6ae3452fa293ccfb805c2cbd4
nov <nov@matake.jp>
2022-06-03 06:45:30 +0900

    sorry, sp model shouldn't needed. we need idp.

--- db/migrate/20220602214134_create_libsaml_service_providers.rb
+++
@@ -1,8 +0,0 @@
-class CreateLibsamlServiceProviders < ActiveRecord::Migration[6.1]
-  def change
-    create_table :libsaml_service_providers do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/models/libsaml/service_provider_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Libsaml::ServiceProviderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
